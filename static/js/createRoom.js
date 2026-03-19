fe10bdd12c2be3c6da19b401d00d88b5ae437b4c
georgii <mygdshowcases@gmail.com>
2026-03-19 10:05:09 +0100

    changed sha256 function, it was glitching in production

--- static/js/createRoom.js
+++ static/js/createRoom.js
@@ -17,7 +17,7 @@ async function getId(){
     setCookie("id", r['personalId']);
 }
 async function sha256(message) {
-    const msgBuffer = new TextEncoder().encode(message);
+    const msgBuffer = await new TextEncoder().encode(message);
     const hashBuffer = await crypto.subtle.digest('SHA-256', msgBuffer);
     const hashArray = Array.from(new Uint8Array(hashBuffer));
     const hashHex = hashArray.map(b => b.toString(16).padStart(2, '0')).join('');
